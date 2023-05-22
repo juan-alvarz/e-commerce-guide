e5b239552f7fa7787713bddce8851333756e9d90
juan-alvarz <juanalvarez0925@gmail.com>
2023-05-22 18:40:59 -0500

    Add configuration for scripts for run lazyOnload

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,25 +1,27 @@
 import Script from "next/script";
+import { useEffect, useState } from "react";
 
 const TRACKING_ID = process.env.NEXT_PUBLIC_GTAG_ID;
 
 export const GoogleAnalytics = () => {
-  const incorporateDataLayer = () => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-
-    gtag("js", new Date());
-    gtag("config", TRACKING_ID);
-  };
-
   return (
     <>
       <Script
+        strategy="lazyOnload"
         async
-        src={`https://www.googletagmanager.com/gtag/js?id={TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
       />
-      {incorporateDataLayer()}
+
+      <Script strategy="lazeOnload" id="gtag-function">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   );
 };
