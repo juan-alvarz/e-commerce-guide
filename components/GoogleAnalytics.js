ba191f1150ce32e78f07c4f2d6b986bbc1c93831
juan-alvarz <juanalvarez0925@gmail.com>
2023-05-22 18:41:24 -0500

    Fix the ID in Script tag

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -17,7 +17,7 @@ export const GoogleAnalytics = () => {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag('config', '${process.env.TRACKING_ID}', {
           page_path: window.location.pathname,
           });
         `}
